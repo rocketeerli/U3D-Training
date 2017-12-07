075642508125be3e60b99b510744d2815d407cd5
FightingStone <13144602916@163.com>
2017-12-07 13:35:46 +0800

    week1

---
+++ week1/BallGame/Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    public int force = 4;
+    private Rigidbody rd;
+    // Use this for initialization
+    void Start () {
+        rd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rd.AddForce(new Vector3(x, 0, z) * force);
+    }
+}
