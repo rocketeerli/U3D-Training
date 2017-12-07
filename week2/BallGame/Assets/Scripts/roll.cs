17541c82655216efbf5723dea8002c2ff69921b2
FightingStone <13144602916@163.com>
2017-12-07 14:13:00 +0800

    week2

---
+++ week2/BallGame/Assets/Scripts/roll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roll : MonoBehaviour {
+    public int speed = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 1, 0) * speed);
+    }
+}
